f24a69534d1772aca05b029d9dcc72c0d8f37bbd
Anokhin.A.Ig <a.i.anokhin.arduino@gmail.com>
2026-04-05 22:43:52 +0300

    init

---
+++ src/main/java/ru/anokhin/dev/onlinegoodsstore/OnlineGoodsStoreApplication.java
@@ -0,0 +1,13 @@
+package ru.anokhin.dev.onlinegoodsstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineGoodsStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineGoodsStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/anokhin/dev/onlinegoodsstore/OnlineGoodsStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.anokhin.dev.onlinegoodsstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineGoodsStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
