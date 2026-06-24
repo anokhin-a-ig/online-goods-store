499e35e1fcf1da225212d8753196a64fa41c1923
Anokhin.A.Ig <a.i.anokhin.arduino@gmail.com>
2026-06-24 23:21:23 +0300

    #36 upgrade registration endpoint: add validation, handle duplicate with 409, set default role

--- src/main/java/ru/anokhin/dev/onlinegoodsstore/dao/RegistrationDao.java
+++ src/main/java/ru/anokhin/dev/onlinegoodsstore/dao/RegistrationDao.java
@@ -1,10 +1,15 @@
 package ru.anokhin.dev.onlinegoodsstore.dao;
 
-import lombok.Data;
-import org.springframework.stereotype.Component;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record RegistrationDao(
+        @NotBlank(message = "Username is required")
+        @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
         String username,
+
+        @NotBlank(message = "Password is required")
+        @Size(min = 6, message = "Password must be at least 6 characters")
         String password
 ) {
 }
