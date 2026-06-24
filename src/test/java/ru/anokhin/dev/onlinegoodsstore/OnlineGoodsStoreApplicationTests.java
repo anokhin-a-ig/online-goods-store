1b3bc898a58ad06eab70ff2fdec14881f8d889b4
Anokhin.A.Ig <a.i.anokhin.arduino@gmail.com>
2026-06-25 00:04:50 +0300

    #33 upd

--- src/test/java/ru/anokhin/dev/onlinegoodsstore/OnlineGoodsStoreApplicationTests.java
+++ src/test/java/ru/anokhin/dev/onlinegoodsstore/OnlineGoodsStoreApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class OnlineGoodsStoreApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
